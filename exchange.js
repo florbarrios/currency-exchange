96893c4efbb942d64e55fae3c80b10da431294d0
Flor Barrios <florbarrios21@gmail.com>
2019-04-02 11:30:55 -0300

    Added date handler. Fixed case where there's no internet and no rates stored.

--- exchange.js
+++ exchange.js
@@ -5,7 +5,14 @@ let GBPrate
 const getExchangeRates = async () => {
     return fetch('https://www.dolarsi.com/api/api.php?type=cotizador')
         .then(r => r.json())
-        .then(data => data)
+        .then(data => {
+            // Saving the last time the data was fetched since the API does not return any dates
+            const timestamp = new Date()
+            const timestampMessage = `Fecha de cotizacion: ${timestamp.getDate()} / ${timestamp.getMonth()} / ${timestamp.getFullYear()}  ${timestamp.getHours()} : ${timestamp.getMinutes()}`
+            localStorage.setItem('timestampMessage', JSON.stringify(timestampMessage))
+
+            return data
+        })
         .catch(err => {
             const ratesJSON = localStorage.getItem('rates')
             const errorMessage = document.createElement('p')
@@ -23,23 +30,24 @@ const getExchangeRates = async () => {
 const setRates = async () => {
     const exchangeRates = await getExchangeRates()
 
-    const USDtoday = exchangeRates.find(function (currency) {
-        return currency.casa.nombre === "Dolar"
-    })
-    USDrate = USDtoday.casa.venta.replace(/,/, ".")
+    if (exchangeRates) {
+        const USDtoday = exchangeRates.find(function (currency) {
+            return currency.casa.nombre === "Dolar"
+        })
+        USDrate = USDtoday.casa.venta.replace(/,/, ".")
 
-    const EURtoday = exchangeRates.find(function (currency) {
-        return currency.casa.nombre === "Euro"
-    })
-    EURrate = EURtoday.casa.venta.replace(/,/, ".")
+        const EURtoday = exchangeRates.find(function (currency) {
+            return currency.casa.nombre === "Euro"
+        })
+        EURrate = EURtoday.casa.venta.replace(/,/, ".")
 
-    const GBPtoday = exchangeRates.find(function (currency) {
-        return currency.casa.nombre === "Libra Esterlina"
-    })
-    GBPrate = GBPtoday.casa.venta.replace(/,/, ".")
+        const GBPtoday = exchangeRates.find(function (currency) {
+            return currency.casa.nombre === "Libra Esterlina"
+        })
+        GBPrate = GBPtoday.casa.venta.replace(/,/, ".")
 
-    localStorage.setItem('rates', JSON.stringify(exchangeRates))
-    
+        localStorage.setItem('rates', JSON.stringify(exchangeRates))
+    }
 }
 
 setRates();
@@ -48,6 +56,11 @@ const getConversion = function (amount, rate) {
     const result = `ARS $ ${amount * rate}`
     const resultEl = document.querySelector('#result-outcome')
     resultEl.innerHTML = result
+
+    const timestampJSON = localStorage.getItem('timestampMessage')
+    const lastTimestamp = JSON.parse(timestampJSON)
+    const timestampEl = document.querySelector('#rate-timestamp')
+    timestampEl.innerHTML = lastTimestamp
 }
 
 document.querySelector('#USD').addEventListener('click', e => {
